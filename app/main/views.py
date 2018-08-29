76c36a5b2733e0281589a38d3a7c3244c17c7078
Tom Chege <emdeechege@gmail.com>
2018-08-29 10:34:27 +0300

    procfile added for heroku

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,19 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .request import get_movies, get_movie, search_movie
-from .models import review
+from . import main
+from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-
-Review = review.Review
+from ..models import Review
+# from flask import render_template,request,redirect,url_for
+# from main import main
+# from .request import get_movies, get_movie, search_movie
+# from .models import review
+# from .forms import ReviewForm
+#
+# Review = review.Review
 
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -29,7 +34,7 @@ def index():
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -41,7 +46,7 @@ def movie(id):
 
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -52,7 +57,7 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=859a79070480e0565c9326e50b3ec49e
+export SECRET_KEY=Flask WTF Secret Key
+
+python3.6 manage.py server
