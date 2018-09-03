1c6b1cf02c30c322531afd4dbcf6a0eb90abf4a8
Tom Chege <emdeechege@gmail.com>
2018-09-03 14:47:57 +0300

    one to many rlshp

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://emdee:Mastime87#@localhost/watchlist'
 
     @staticmethod
     def init_app(app):
